c0bcbd5d4d0b7b71da0f2c0d1f797e6bc071fc5e
TibiSzabo29 <robert.tiberiu.szabo@stud.ubbcluj.ro>
2026-06-12 19:28:04 +0300

    Hashed password

--- server-dotnet/DigitalGradebook.Repository/Migrations/20260612160914_InitialCreate.cs
+++ server-dotnet/DigitalGradebook.Repository/Migrations/20260612160914_InitialCreate.cs
@@ -254,8 +254,8 @@ namespace DigitalGradebook.Repository.Migrations
             migrationBuilder.Sql("INSERT INTO \"Roles\" (\"Name\") VALUES ('Student');");
             migrationBuilder.Sql("INSERT INTO \"Roles\" (\"Name\") VALUES ('Parent');");
             migrationBuilder.Sql("INSERT INTO \"Roles\" (\"Name\") VALUES ('Admin');");
-            migrationBuilder.Sql("INSERT INTO \"Users\" (\"Username\", \"PasswordHash\", \"RoleId\", \"SecurityPinHash\") SELECT 'admin', '$2a$11$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', \"Id\", '$2a$11$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' FROM \"Roles\" WHERE \"Name\" = 'Teacher';");
-            migrationBuilder.Sql("INSERT INTO \"Users\" (\"Username\", \"PasswordHash\", \"RoleId\", \"SecurityPinHash\") SELECT 'superadmin', '$2a$11$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', \"Id\", '$2a$11$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' FROM \"Roles\" WHERE \"Name\" = 'Admin';");
+            migrationBuilder.Sql("INSERT INTO \"Users\" (\"Username\", \"PasswordHash\", \"RoleId\", \"SecurityPinHash\") SELECT 'admin', '$2a$11$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', \"Id\", '$2a$11$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy' FROM \"Roles\" WHERE \"Name\" = 'Teacher';");
+            migrationBuilder.Sql("INSERT INTO \"Users\" (\"Username\", \"PasswordHash\", \"RoleId\", \"SecurityPinHash\") SELECT 'superadmin', '$2a$11$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', \"Id\", '$2a$11$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy' FROM \"Roles\" WHERE \"Name\" = 'Admin';");
         }
 
         /// <inheritdoc />
